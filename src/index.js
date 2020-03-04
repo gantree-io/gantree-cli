b4e2fc4e10cad655dd4c33c87fdac3c6d0782030
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-04 18:13:28 +1100

    clean up CLI script

--- src/index.js
+++ src/index.js
@@ -2,44 +2,22 @@
 
 const program = require('commander')
 const version = require('./lib/version')
+const { syncWrapper } = require('./cli/syncWrapper')
+const { cleanWrapper } = require('./cli/cleanWrapper')
 
 program.version(version.show())
 
-// program
-//   .command('sync')
-//   .description('Synchronizes the infrastructure.')
-//   .option('-c, --config [config] (required)', 'Path to config file.')
-//   .action(sync.do)
+program
+  .command('sync')
+  .description('Synchronizes the infrastructure.')
+  .option('-c, --config [config] (required)', 'Path to config file.')
+  .action(syncWrapper)
 
-// program
-//   .command('clean')
-//   .description('Removes all the resources.')
-//   .option('-c, --config [config] (required)', 'Path to config file.')
-//   .action(clean.do)
-
-// program
-//   .command('inject')
-//   .description('Injects keys into chainspec (non-raw).')
-//   .option('-c, --chainspec [path]', 'Path to chainspec file.')
-//   .option('-v, --validatorspec [path]', 'Path to validatorspec file.')
-//   .option(
-//     '-R, --allow-raw',
-//     'Allow usage of raw chainspec (returns raw chainspec in stdout)',
-//     false
-//   )
-//   .action(inject.do)
-
-// program
-//   .command('key-combine')
-//   .description(
-//     'Combines multiple validator key yaml into a single validator key json file.'
-//   )
-//   .option(
-//     '-d, --directory [path]',
-//     'Path to session directory.',
-//     '/tmp/gantree_host/session'
-//   )
-//   .action(keyCombine.do)
+program
+  .command('clean')
+  .description('Removes all the resources.')
+  .option('-c, --config [config] (required)', 'Path to config file.')
+  .action(cleanWrapper)
 
 program.allowUnknownOption(false)
 
