52debb1445bd88523ad5d63958779c8df12a9a0f
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-16 13:45:12 +1100

    [art] print gantree title on ansibleSync and ansibleClean

--- src/cli/ansibleCleanWrapper.js
+++ src/cli/ansibleCleanWrapper.js
@@ -1,4 +1,5 @@
 const { Gantree, packageDir } = require('gantree-lib')
+const { gantreeTitle } = require('../lib/art')
 const path = require('path')
 const inventoryPath = path.join(packageDir, '/inventory')
 const ansiblePath = path.join(packageDir, '/ansible')
@@ -6,6 +7,7 @@ const ansiblePath = path.join(packageDir, '/ansible')
 const gantree = new Gantree()
 
 async function ansibleCleanWrapper() {
+  console.log(gantreeTitle)
   console.warn('Please note, ansibleClean is a temporary subcommand.')
   await gantree.ansibleCleanAll(inventoryPath, ansiblePath)
 }
--- src/cli/ansibleSyncWrapper.js
+++ src/cli/ansibleSyncWrapper.js
@@ -1,4 +1,5 @@
 const { Gantree, packageDir } = require('gantree-lib')
+const { gantreeTitle } = require('../lib/art')
 const path = require('path')
 const inventoryPath = path.join(packageDir, '/inventory')
 const ansiblePath = path.join(packageDir, '/ansible')
@@ -6,6 +7,7 @@ const ansiblePath = path.join(packageDir, '/ansible')
 const gantree = new Gantree()
 
 async function ansibleSyncWrapper() {
+  console.log(gantreeTitle)
   console.warn('Please note, ansibleSync is a temporary subcommand.')
   await gantree.ansibleSyncAll(inventoryPath, ansiblePath)
 }
