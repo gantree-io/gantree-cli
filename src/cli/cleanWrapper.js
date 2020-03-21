5f5f7ac980dbfab6be3e5ac44900dbd90203fc38
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-21 18:20:41 +1100

    remove ansible prefix from command wrappers

--- src/cli/cleanWrapper.js
+++ src/cli/cleanWrapper.js
@@ -1,16 +1,15 @@
-const { Gantree, throwGantreeError } = require('gantree-lib')
+const { Gantree, packageDir } = require('gantree-lib')
+const { gantreeTitle } = require('../lib/art')
+const path = require('path')
+const inventoryPath = path.join(packageDir, '/inventory')
+const ansiblePath = path.join(packageDir, '/ansible')
 
 const gantree = new Gantree()
 
-async function cleanWrapper(cmd) {
-  if (cmd.config === undefined) {
-    throwGantreeError(
-      'MISSING_ARGUMENTS',
-      Error('Please specify the path to your gantree config')
-    )
-  }
-  const my_config = await gantree.returnConfig(cmd.config)
-  await gantree.cleanPlatform(my_config)
+async function cleanWrapper() {
+  console.log(gantreeTitle)
+  console.warn('[!] Please note, this subcommand is still in beta\n')
+  await gantree.cleanAll(inventoryPath, ansiblePath)
 }
 
 module.exports = {
--- src/cli/syncWrapper.js
+++ src/cli/syncWrapper.js
@@ -1,16 +1,14 @@
-const { Gantree, throwGantreeError } = require('gantree-lib')
+const { Gantree } = require('gantree-lib')
+const { gantreeTitle } = require('../lib/art')
 
 const gantree = new Gantree()
 
-async function syncWrapper(cmd) {
-  if (cmd.config === undefined) {
-    throwGantreeError(
-      'MISSING_ARGUMENTS',
-      Error('Please specify the path to your gantree config')
-    )
-  }
-  const my_config = await gantree.returnConfig(cmd.config)
-  await gantree.syncAll(my_config)
+async function syncWrapper() {
+  console.log(gantreeTitle)
+  console.warn('[!] Please note, this subcommand is still in beta\n')
+  const gantreeConfigPath = process.env.GANTREE_CONFIG_PATH
+  const gantreeConfigObj = await gantree.returnConfig(gantreeConfigPath)
+  await gantree.syncAll(gantreeConfigObj)
 }
 
 module.exports = {
