ae34c6deb83c458dc22ea562632cdee907c1ef61
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-03-31 18:34:21 +1100

    [precommit] update ver_check.js

--- ver_check.js
+++ ver_check.js
@@ -9,12 +9,18 @@ const libVersionSatisfactory = semver.satisfies(
   requiredLibVersion
 )
 
+const red = '\x1b[31m'
+const yellow = '\x1b[33m'
+
 if (libVersionSatisfactory !== true) {
-  console.log(`(lib)      real version: ${realLibVersion}`)
-  console.log(`(lib)  required version: ${requiredLibVersion}`)
-  console.log(`(lib) semvar satisfied?: ${libVersionSatisfactory}`)
-  console.log('Library version not satisfied!')
-  console.error('Library version not satisfied!')
+  console.error(
+    `gantree-lib version doesn't satisfy semvar pattern in dependencies (pattern: ${requiredLibVersion}, local version: ${realLibVersion})`
+  )
+  console.log(
+    `${red}issue: gantree-lib version doesn't satisfy semvar pattern in dependencies (pattern: ${requiredLibVersion}, local version: ${realLibVersion})`
+  )
+  console.log(`${yellow}(lib)      real version: ${realLibVersion}`)
+  console.log(`${yellow}(lib)  required version: ${requiredLibVersion}`)
   process.exit(1)
 } else {
   console.log('Library version satisfied.')
